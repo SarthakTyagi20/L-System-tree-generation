94f30668272daef743c4b24d6b4fdbe9b630e99d
SarthakTyagi20 <85821901+SarthakTyagi20@users.noreply.github.com>
2022-12-20 10:42:17 +0530

    Update main.cpp

--- treeGen_lsys/src/main.cpp
+++ treeGen_lsys/src/main.cpp
@@ -23,19 +23,26 @@ int main()
             // cout<<"3";
         sleep(1); 
         int display_w, display_h;
+        
+        //setting up opengl template
         glfwGetFramebufferSize(window, &display_w, &display_h);
         glViewport(0, 0, display_w, display_h);
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
          
         if (l.depth1 < levels) {
+            //generating sequence accoding to the levels (size)
             l.depth1 = l.depth1 + 1;
+            
+            //for each levels we will have a different tree, we've made this to give effect of 
+            //growing tree 
             l.create_sequence("X", l.depth1, 0);
             // cout<<l.genStr; 
         }
         
         // cout<<l.genStr;
-
+        
+        //calling this function to render the tree 
         l.visualise_sequence(l.genStr);
 
         glfwSwapBuffers(window);
